346326bc7e699bc8df6f40a0241a07dd2b0a5211
Alexander Wolf <alexander.wolf@8mylez.com>
2020-01-03 00:03:41 +0100

    Finish basic functionality
    If the dropdown opens the input gets focused. The input event handles the filtering for the labels of the list items.

--- src/Resources/app/storefront/src/frosh-platform-filter-search/frosh-platform-filter-search.plugin.js
+++ src/Resources/app/storefront/src/frosh-platform-filter-search/frosh-platform-filter-search.plugin.js
@@ -1,7 +1,41 @@
 import Plugin from 'src/plugin-system/plugin.class';
 
 export default class FroshPlatformSearchFilterPlugin extends Plugin {
+    static options = {
+        optionListSizeTrigger: 10,
+        dropdownSelector: '#filter-panel-wrapper .dropdown',
+    };
+
     init() {
-        console.log('WHAT UP?');
+        this._registerEvents();
+    }
+
+    _registerEvents() {
+        this.el.addEventListener('input', this._onInput.bind(this));
+        $(this.options.dropdownSelector).on('shown.bs.dropdown', this._onDropdownShown.bind(this));
+    }
+
+    _onInput(event) {
+        const value = event.target.value;
+
+        const listItems = event.target
+            .closest('.filter-multi-select-dropdown')
+            .querySelector('.filter-multi-select-list')
+            .querySelectorAll('li');
+
+        const listArray = Array.from(listItems);
+
+        listArray.forEach(item => {
+            item.style.display = 'none';
+            const label = item.querySelector('.filter-multi-select-item-label').innerHTML.trim().toLowerCase();
+
+            if (label.includes(value)) {
+                item.style.display = 'list-item';
+            }
+        });
+    }
+
+    _onDropdownShown(event) {
+        event.relatedTarget.closest('.dropdown').querySelector('[data-frosh-platform-filter-search=true]').focus();
     }
 }
